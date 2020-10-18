7ebefc69730de4d63032f75964b2a22ee046abf3
vinay k <reddyvinay172@gmail.com>
2020-10-18 13:45:57 +0530

    callbacks and errorhandling example

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-var rect={
-    perimete:(x,y)=>(2*(x+y)),
-    area:(x,y)=>(x*y)
-};
+var rect=('./rectangle')
 
 
 function solve( l,b )
 {
-    if(l<=0 || b<=0 )console.log("check dimensions");
-    else
-    
-    {console.log(rect.perimete(l,b));
-          console.log(rect.area(l,b));
+    rect(l,b,(err,rectangle)=>
+   { if(err)console.log("ERROR" +err.message);
+    else{
+        console.log(" area of recatnagle with l= "+l,"breadth b= "+b +" ="+ rectangle.area());
+        console.log(" perimeter of recatnagle with l= "+l,"breadth b= "+b +" ="+ rectangle.perimeter());
     }
-
+    console.log("this should print after above calculation ");
 }
+    )
 solve(0,2);
 solve(3,4);
